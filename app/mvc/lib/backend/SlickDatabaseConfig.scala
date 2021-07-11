29a8991462d5d2d48e27b3681073c7626fbf6a52
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-07-11 19:50:45 +0900

    Create SlickDatabaseConfig

--- app/mvc/lib/backend/SlickDatabaseConfig.scala
+++ app/mvc/lib/backend/SlickDatabaseConfig.scala
@@ -1,2 +1,15 @@
 package mvc.lib.backend
 
+import slick.dbio.{DBIOAction, NoStream}
+import slick.jdbc.JdbcBackend.Database
+
+import scala.concurrent.Future
+
+trait SlickDatabaseConfig {
+
+  private def db(replication: String): Database = Database.forConfig(s"slick.dbs.${replication}.db")
+
+  def slickDBAction[R](replication: String, action: DBIOAction[R, NoStream, Nothing]): Future[R] = db(replication).run(action)
+}
+
+object SlickDatabaseConfig extends SlickDatabaseConfig
