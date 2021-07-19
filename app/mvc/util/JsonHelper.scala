5a79dfe2eaada1a17acdbf5311fb1ab84901bc29
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-07-19 21:49:08 +0900

    Fix JsonHelper

--- app/mvc/util/JsonHelper.scala
+++ app/mvc/util/JsonHelper.scala
@@ -3,16 +3,33 @@ package mvc.util
 import play.api.libs.json._
 import play.api.mvc.Results._
 import play.api.mvc._
+import play.api.Logging
 
-object JsonHelper {
+import mvc.api.util.JsValueAPIError
+
+object JsonHelper extends Logging{
 
   def bindFromRequest[T](implicit request: Request[AnyContent], reads: Reads[T]): Either[Result, T] = {
     request.body.asJson match {
       case Some(json) => json.validate[T] match {
         case JsSuccess(v, _) => Right(v)
-        case JsError(error)  => Left(BadRequest)
+        case JsError(error)  => Left {
+          logger.error(JsError.toJson(error).toString())
+          BadRequest(Json.toJson(
+            JsValueAPIError(
+              message = "Illegal value was detected in the transmitted data",
+              detail  = Some(JsError.toJson(error))
+            )
+          ))
+        }
       }
-      case None => Left(BadRequest)
+      case None => Left {
+        BadRequest(Json.toJson(
+          JsValueAPIError(
+            message = "Illegal value was detected in the transmitted data"
+          )
+        ))
+      }
     }
   }
 }
